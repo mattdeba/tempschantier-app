80fca693c0a390d9326302b9714d62d35c22edcf
matthieu debarge <debarge.matthieu@gmail.com>
2026-03-01 16:17:39 +0100

    charger primeng

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,33 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
+import {providePrimeNG} from "primeng/config";
+import Aura from '@primeng/themes/aura';
+import {definePreset} from "@primeng/themes";
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{orange.50}',
+      100: '{orange.100}',
+      200: '{orange.200}',
+      300: '{orange.300}',
+      400: '{orange.400}',
+      500: '{orange.500}',
+      600: '{orange.600}',
+      700: '{orange.700}',
+      800: '{orange.800}',
+      900: '{orange.900}',
+      950: '{orange.950}'
+    }
+  }
+});
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [provideAnimations(),
+    providePrimeNG({
+    theme: {
+      preset: MyPreset
+    }
+  })]
+});
